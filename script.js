2dbe19514698969ee5f19f6c13f401d381934c65
Filip Stangricki <stangricki@gmail.com>
2017-12-11 20:06:53 +0100

    Quote generator logic added

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-var url = 'http://api.icndb.com/jokes/random';
-var button = document.getElementById('get-joke');
-button.addEventListener(
-	'click', 
-	function() {getJoke();}
-	);
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-var paragraph = document.getElementById('joke');
+function getQuote() {
+	$.getJSON(prefix + quoteUrl, createTweet);
+	$.ajaxSetup({ cache: false });
+};
 
-function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'http://api.icndb.com/jokes/random');
-	xhr.addEventListener('load', function(){
-	 	var response = JSON.parse(xhr.response);
-	 	paragraph.innerHTML = response.value.joke;
-	 });
-	xhr.send();
+function createTweet(input) {
+    var data = input[0];
+
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+   if (tweetText.length > 140) {
+	    getQuote();
+	} else {
+	    var tweet = tweetLink + encodeURIComponent(tweetText);
+	    $('.quote').text(quoteText);
+	    $('.author').text(quoteAuthor);
+	    $('.tweet').attr('href', tweet);
+	}
+	$('.tweet').attr('href', tweet);
 }
 
-getJoke()
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    })
+});
